0e3b175b01b179146fea85383b101bd746d567aa
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2023-10-22 22:09:15 -0300

    Implement initial data

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,7 +3,10 @@ import { UserDto } from './dto/user.dto/user.dto';
 
 @Injectable()
 export class UserService {
-  private users: UserDto[] = [];
+  private users: UserDto[] = [
+    { id: 1, name: 'John', lastName: 'Doe' },
+    { id: 2, name: 'Mary', lastName: 'Weather' },
+  ];
 
   create(user: UserDto) {
     this.users.push(user);
