7801e274284552dea01cba183e5052aaf37ad259
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2023-10-22 21:54:28 -0300

    Implements create and findAll on UserService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { UserDto } from './dto/user.dto/user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private users: UserDto[] = [];
+
+  create(user: UserDto) {
+    this.users.push(user);
+  }
+
+  findAll() {
+    return this.users;
+  }
+}
