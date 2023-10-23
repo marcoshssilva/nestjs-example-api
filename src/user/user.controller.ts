cc3a6e0fcc365304f3f2fdb4626319fee8e102df
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2023-10-22 22:00:23 -0300

    Create Post and Get http methods to Users endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserDto } from './dto/user.dto/user.dto';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post('/')
+  create(@Body() user: UserDto) {
+    this.userService.create(user);
+    return 'CREATED';
+  }
+
+  @Get('/')
+  findAll() {
+    return this.userService.findAll();
+  }
+}
